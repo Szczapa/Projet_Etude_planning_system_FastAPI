af0c041cc5a35963e0d95d2c12a3e25c2a70e1dc
Szczapa <98310905+Szczapa@users.noreply.github.com>
2023-05-11 22:45:05 +0200

    Ajout de la création de company

--- app/modeles/company.py
+++ app/modeles/company.py
@@ -1,8 +1,11 @@
 from db.db_inc import engine, SessionLocal, Base
 from sqlalchemy import Column, Integer, String
-
+from pydantic import BaseModel
 
 class Company(Base):
     __tablename__ = "companies"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), unique=True, index=True)
+
+class CompanyCreate(BaseModel):
+    name: str
--- app/routers/company.py
+++ app/routers/company.py
@@ -1,14 +1,19 @@
-from fastapi import APIRouter, status, HTTPException, Response
+from fastapi import APIRouter, status, HTTPException, Response, Depends
 from db.db_inc import engine, Base
-from modeles.company import Company
+from modeles.company import Company, CompanyCreate
 from sqlalchemy.orm import sessionmaker
+from routers.login import get_current_user
+from modeles.user import User
+from sqlalchemy import func
 
 router = APIRouter()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 @router.get("/companies")
-def read_companies():
+def read_companies(current_user: User = Depends(get_current_user)):
+    if current_user.role_id != 3:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
     db = SessionLocal()
     companies = db.query(Company).all()
     return companies
@@ -19,3 +24,52 @@ def read_companies():
 #     db = SessionLocal()
 #     company = db.query(Company).filter(Company.id == company_id).first()
 #     return company
+
+
+@router.post("/companies", status_code=status.HTTP_201_CREATED)
+def create_company(company: CompanyCreate, current_user: User = Depends(get_current_user)):
+    if current_user.role_id != 3:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    db = SessionLocal()
+    existing_company = db.query(Company).filter(func.lower(Company.name) == func.lower(company.name)).first()
+    if existing_company:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Company name already exists")
+
+    new_company = Company(
+        name=company.name
+    )
+    db.add(new_company)
+    db.commit()
+    db.refresh(new_company)
+    return new_company
+
+@router.delete("/companies/{company_id}")
+def delete_company(company_id: int, current_user: User = Depends(get_current_user)):
+    if current_user.role_id != 3:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    db = SessionLocal()
+    company = db.query(Company).filter(Company.id == company_id).first()
+    if company is None:
+        raise HTTPException(status_code=204, detail="Company not found")
+    db.delete(company)
+    db.commit()
+    return {"message": "Company deleted successfully"}
+
+@router.put("/companies/{company_id}")
+def update_company(company_id: int, company: CompanyCreate, current_user: User = Depends(get_current_user)):
+    if current_user.role_id != 3:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    db = SessionLocal()
+    existing_company = db.query(Company).filter(func.lower(Company.name) == func.lower(company.name)).first()
+    if existing_company:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Company name already exists")
+
+    company_to_update = db.query(Company).filter(Company.id == company_id).first()
+    if company_to_update is None:
+        raise HTTPException(status_code=204, detail="Company not found")
+    company_to_update.name = company.name
+    db.commit()
+    return {"message": "Company updated successfully"}
