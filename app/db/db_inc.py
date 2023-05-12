e0229bdfd71f313cf3fc89ca345f9bf9a782c49c
Szczapa <gilabertadam@gmail.com>
2023-05-12 13:30:06 +0200

    systeme de base de donnée

--- app/db/db_inc.py
+++ app/db/db_inc.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@host.docker.internal/teamwork_management"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@host.docker.internal/teamwork_management"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@db/teamwork_management"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
