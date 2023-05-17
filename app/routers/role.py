16f42cc2b407fadd8316d514a718ffb3438e22bb
Szczapa <gilabertadam@gmail.com>
2023-05-17 12:55:25 +0200

    suppression diverse role

--- app/routers/role.py
+++ app/routers/role.py
@@ -16,9 +16,3 @@ def read_roles(current_user: Role = Depends(get_current_user)):
     roles = db.query(Role).all()
     return roles
 
-
-@router.get("/roles/{role_id}")
-def read_role(role_id: int):
-    db = SessionLocal()
-    role = db.query(Role).filter(Role.id == role_id).first()
-    return role
